fc1546491d7448751897ceeea31844698a22e240
Sladjan-Stojakovic <stojakovicsladjan@gmail.com>
2024-08-20 13:23:18 +0200

    Dodati Kontroleri i rute za Home i Resident

--- smarthome_lar/routes/api.php
+++ smarthome_lar/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ResidentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/homes', [HomeController::class, 'index']);
+Route::get('/homes/{id}', [HomeController::class, 'show']);
+
+Route::get('/residents', [ResidentController::class, 'index']);
+Route::get('/residents/{id}', [ResidentController::class, 'show']);
+
 
 Route::post('/register', [LoginController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
@@ -24,6 +32,14 @@ oute::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
+
+    Route::resource('/homes', HomeController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/residents', ResidentController::class)
+        ->only(['store', 'update', 'destroy']);
+
+
 
 
 
